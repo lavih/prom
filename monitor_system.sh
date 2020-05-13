fd7e597a7499fdd040e1bffe8b54ad362f4ed673
lavih <lavih44@walla.co.il>
2020-05-13 18:56:15 +0300

    prom

--- monitor_system.sh
+++ monitor_system.sh
@@ -1,8 +1,8 @@
-while true; do
-free -h
-df -ah
-cpu usage
-iostats
-echo $(free -h) | nc -w 1 -u 127.0.0.1 8125
-sleep 1
-done
+#while true; do
+#free -h
+#df -ah
+#cpu usage
+#iostats
+#echo $(free -h) | nc -w 1 -u 127.0.0.1 8125
+#sleep 1
+#done
