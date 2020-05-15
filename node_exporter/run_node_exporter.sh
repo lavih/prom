7bcbbe3cefccd1eea61003fb73c1cf2cff002751
lavih <lavih44@walla.co.il>
2020-05-15 16:04:48 +0300

    prom

--- node_exporter/run_node_exporter.sh
+++ node_exporter/run_node_exporter.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./node_exporter --web.listen-address="0.0.0.0:9100" > /dev/null 2>&1 &
+echo PROMETHEUS_HOST=$(ip addr | grep "global dynamic" | awk '{print $2}' | sed 's/\/24//g' ) > ../.env
 
--- prometheus/get_prometheus_ip.sh
+++
@@ -1,2 +0,0 @@
-echo PROMETHEUS_HOST=$(ip addr | grep "global dynamic" | awk '{print $2}' | sed 's/\/24//g' ) > ../.env
-
