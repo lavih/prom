8c8cb2bd2d2b82381777797404085683c44937ad
lavih <lavih44@walla.co.il>
2020-05-16 21:26:31 +0300

    porm

--- node_exporter/run_node_exporter.sh
+++ node_exporter/run_node_exporter.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ./node_exporter --web.listen-address="0.0.0.0:9100" > /dev/null 2>&1 &
 echo PROMETHEUS_HOST=$(ip addr | grep "global dynamic" | awk '{print $2}' | sed 's/\/24//g' ) > ../.env
+sed -i "s/PROMETHEUS_HOST/$HOSTNAME/g" ../prometheus/targets.json
 
