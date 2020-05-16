1c9cc6fd18c7adcfec687697ab973f1d9abc9892
lavih <lavih44@walla.co.il>
2020-05-16 23:58:16 +0300

    prom

--- collectd/run_install_collectd.sh
+++ collectd/run_install_collectd.sh
@@ -1,7 +1,7 @@
 apt update -y
 apt install collectd -y
+GRAPHITE_HOST=$(hostname)
+sed -i "s/GRAPHITE_HOST/$GRAPHITE_HOST/g" collectd.conf
 cp -f ./collectd.conf /etc/collectd/collectd.conf
 #export GRAPHITE_HOST=$(ip addr | grep "global dynamic" | awk '{print $2}' | sed 's/\/24//g' )
 systemctl start collectd
-systemctl enable collectd
-
